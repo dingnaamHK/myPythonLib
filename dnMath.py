fe796254a463900227f38eb9aaf32911e4a362df
KL5163 HK <klproductionhk@gmail.com>
2024-06-18 09:22:37 +0800

    function "add" and "mul"

---
+++ dnMath.py
@@ -0,0 +1,17 @@
+class sim():
+    def __init__(self, *addargs):
+        self.addargs = list()
+        for i in addargs:
+            self.addargs.append(i)
+        
+    def add(self):
+        count = 0
+        for j in self.addargs:
+            count += j
+        return count
+    
+    def mul(self):
+        count = 1
+        for k in self.addargs:
+            count *= k
+        return count
---
+++ test.py
@@ -0,0 +1,9 @@
+import dnMath
+
+x = 3
+y = 9
+z = 11
+
+num = dnMath.sim(x, y, z)
+print(num.add())
+print(num.mul())
